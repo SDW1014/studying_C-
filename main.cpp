11af2a20b43829f6b1fae215c4fb642c8fc013e2
SDW1014 <apif730@gmail.com>
2024-04-24 00:41:00 +0900

    [feat] 2024-04-24 람다 형식을 사용한 연습

--- main.cpp
+++ main.cpp
@@ -1,45 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-class shape
-{
-public :
-	virtual double getArea() const = 0;
-};
-
-class Circle : public shape
-{
-private:
-	double radius;
-public:
-	Circle(double r) : radius(r) {}
-	double getArea() const override
-	{
-		return 3.141592 * radius * radius;
-	}
-};
-class Rectangle : public shape
-{
-private:
-	double width;
-	double height;
-
-public:
-	Rectangle(double w, double h) : width(w), height(h) {}
-	double getArea() const override
-	{
-		return width * height;
-	}
-};
-
 int main()
 {
-	Circle c(5.0);
-	Rectangle r(4.0, 6.0);
+	vector<int> numbers = {1,2,3,4,5,6,7,8,9,10};
+	
+	vector<int> evenNumbers;
 
-	cout << "Circle area : " << c.getArea() << endl;
-	cout << "Rectangle area : " << r.getArea() << endl;
+	copy_if(numbers.begin(), numbers.end(), back_inserter(evenNumbers), [](int n)
+	{
+		return !(n % 2);
+	});
 
+	cout << "Even Numbers : ";
+
+	for (int n : evenNumbers)
+	{
+		cout << n << endl;
+	}
+	cout << endl;
 	return 0;
 }
+
