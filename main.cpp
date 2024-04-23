4b78eb9f078e55c72cdf2ed960b3b3bce7af1dba
SDW1014 <apif730@gmail.com>
2024-04-23 23:20:28 +0900

    [feat] main인데, pointer를 연습했다.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,41 @@
 #include <iostream>
-#include <vector>
-#include "Animal20240421.h"
-#include "Dog20240421.h"
-#include "Cat20240421.h"
 
-using namespace std;
-
+void updateValue(int& ref)
+{
+	ref += 10;
+}
 
 int main()
 {
-	vector<Animal20240421*> vAnimal;
+	int number = 10;
+	int* ptr = &number;
+	
+	std::cout << "Original number : " << number << std::endl;
 
-	vAnimal.push_back(new Animal20240421());
-	vAnimal.push_back(new Dog20240421());
-	vAnimal.push_back(new Cat20240421());
+	*ptr = 20;
+	
+	std::cout << "Modified number : " << number << std::endl;
 
-	for (auto& animal : vAnimal)
+	int arr[3] = {1,2,3};
+	int* arr_ptr = arr;
+
+	std::cout << "Array elements : ";
+	for (int i = 0; i < 3; i++)
 	{
-		animal->speak();
+		std::cout << *(arr_ptr + i) << " ";
 	}
+	std::cout << std::endl;
 
-	for (auto& animal : vAnimal)
-	{
-		delete animal;
-	}
+	int number2 = 10;
+	int& ref2 = number2;
 
-	vAnimal.clear();
+	std::cout << "Original number2 : " << number2 << std::endl;
+
+	ref2 = 20;
+	std::cout << "Modified number2 : " << number2 << std::endl;
+
+	updateValue(number2);
+	std::cout << "Updated number after function call : " << number2 << std::endl;
 
 	return 0;
 }
