eab379ce840011c6427b78f5d285dbac248b9e76
SDW1014 <apif730@gmail.com>
2024-04-23 23:23:33 +0900

    [feat] 기본적인 char형을 사용한 배열 사실상 string이랑 비슷하다

--- main.cpp
+++ main.cpp
@@ -1,41 +1,17 @@
 #include <iostream>
-
-void updateValue(int& ref)
-{
-	ref += 10;
-}
+#include <cstring>
 
 int main()
 {
-	int number = 10;
-	int* ptr = &number;
-	
-	std::cout << "Original number : " << number << std::endl;
+	char* a = new char[100];
 
-	*ptr = 20;
-	
-	std::cout << "Modified number : " << number << std::endl;
+	strcpy_s(a, sizeof(a), "Hello");
 
-	int arr[3] = {1,2,3};
-	int* arr_ptr = arr;
+	strcat_s(a, sizeof(a), ", World!");
 
-	std::cout << "Array elements : ";
-	for (int i = 0; i < 3; i++)
-	{
-		std::cout << *(arr_ptr + i) << " ";
-	}
-	std::cout << std::endl;
+	std::cout << a << std::endl;
 
-	int number2 = 10;
-	int& ref2 = number2;
-
-	std::cout << "Original number2 : " << number2 << std::endl;
-
-	ref2 = 20;
-	std::cout << "Modified number2 : " << number2 << std::endl;
-
-	updateValue(number2);
-	std::cout << "Updated number after function call : " << number2 << std::endl;
+	delete[] a;
 
 	return 0;
 }
