6379aa69d1592ed8e56cf207eb2e9c63c1eafd44
SDW1014 <apif730@gmail.com>
2024-04-28 02:01:27 +0900

    [feat] 2024-04-28 : add으로 더하기를 해보았다.

--- main.cpp
+++ main.cpp
@@ -1,29 +1,19 @@
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-void bubbleSortWithVector(vector<int>& abc)
+int add(int a, int b)
 {
-	for (int i = 0; i < abc.size() - 1; i++)
-	{
-		for (int j = 0; j < abc.size() - 1 - i; j++)
-		{
-			if (abc[j] > abc[j + 1])
-			{
-				int temp = abc[j];
-				abc[j] = abc[j + 1];
-				abc[j + 1] = temp;
-			}
-		}
-	}
+	return a + b;
 }
 
 int main()
 {
-	vector<int> vecInteger = { 1,2,32,134,124,124,15,234,5324,5234,523,4523,44523,452,345 };
-	bubbleSortWithVector(vecInteger);
+	int x = 5;
+	int y = 3;
 
-	for (auto& element : vecInteger)
-		cout << element << endl;
+	int result = add(x, y);
+	cout << "The sum is: " << result << endl;
 	return 0;
-} 
+}
