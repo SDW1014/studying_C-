cf3d3ed9af463045997489bc1bf078f606241f94
SDW1014 <apif730@gmail.com>
2024-04-28 01:59:45 +0900

    [feat] 2024-04-28 : bubleSort() 를 연습

--- main.cpp
+++ main.cpp
@@ -1,15 +1,29 @@
-#include "GcdAndLcm20240426.h"
+#include <iostream>
 #include <vector>
-
 using namespace std;
+
+void bubbleSortWithVector(vector<int>& abc)
+{
+	for (int i = 0; i < abc.size() - 1; i++)
+	{
+		for (int j = 0; j < abc.size() - 1 - i; j++)
+		{
+			if (abc[j] > abc[j + 1])
+			{
+				int temp = abc[j];
+				abc[j] = abc[j + 1];
+				abc[j + 1] = temp;
+			}
+		}
+	}
+}
 
 int main()
 {
-	GcdAndLcm20240426 GCD(3,5);
-	cout << GCD.gcd() << endl;
-	for (auto& num : GCD.getVec())
-	{
-		cout << num << endl;
-	}
+	vector<int> vecInteger = { 1,2,32,134,124,124,15,234,5324,5234,523,4523,44523,452,345 };
+	bubbleSortWithVector(vecInteger);
+
+	for (auto& element : vecInteger)
+		cout << element << endl;
 	return 0;
-}
+} 
