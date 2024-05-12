acd221f51b4166bae283cde0d70cbb91144c19c0
SDW1014 <apif730@gmail.com>
2024-05-12 14:04:12 +0900

    [feat] 2024-05-02 이전 cmath Test

--- main.cpp
+++ main.cpp
@@ -1,64 +1,28 @@
 #include <iostream>
-#include <random>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-	int counter = 0;
-	cout << "출력할 숫자의 범위를 입력해주세요 : ";
-	cin >> counter;
+	int number_turns;
+	cout << "나선을 몇 바퀴 그릴까요? ";
+	cin >> number_turns;
 
-	int index = 0;
+	double golden_ratio = (1 + sqrt(5)) / 2;
+	double theta = 0;
+	double radius = 1;
 
-	while (index <= counter)
+	while (number_turns-- > 0)
 	{
-		if (index % 2 == 0)
-			cout << index <<endl;
-		index++;
+		double x = radius * cos(theta);
+		double y = radius * sin(theta);
+
+		cout << "(" << x << ", " << y << ")" << endl;
+
+		radius *= golden_ratio;
+
+		theta += asin(1 / golden_ratio);
 	}
-
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> dis(1, 100);
-
-	int quetion_number = dis(gen);
-
-	cout << "question_number : " << quetion_number << endl;
-
-	int result_number = 101;
-	while (result_number != quetion_number)
-	{
-		cout << "input result number (0 <= x <= 100) : ";
-
-		cin >> result_number;
-	}
-
-	cout << "this number is correct" << endl;
-
-	int n;
-	cout << "피보나치의 길이를 입력하세요 n :";
-	cin >> n;
-
-	int a = 1, b = 1;
-
-	cout << "피보나치 수열 :  " << endl;
-
-	if (n >= 1)
-		cout << a << " ";
-	if (n >= 2)
-		cout << b << " ";
-
-	int count = 3;
-	while (count <= n)
-	{
-		int c = a + b;
-		cout << c << " ";
-		a = b;
-		b = c;
-
-		count++;
-	}
-
 	return 0;
 }
